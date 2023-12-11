084d0b07e1b1d3fe1490e680d628c98888763d3d
wajahatarajput <wajahatarajput@gmail.com>
2023-12-11 14:53:39 +0500

    users fs

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3180;
 var cors = require('cors');
-
 app.use(cors());
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({ limit: '200MB' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '200MB', parameterLimit: 20000 }));
 app.use(bodyParser.raw()); // Change '5mb' to your desired limit
+const fs = require('fs');
 
 var postData = [];
-var users = [];
 
 app.get('/getpostdata', (req, res) => {
   res.send(JSON.parse(JSON.stringify(postData)))
@@ -50,6 +49,61 @@ app.put('/updatepost', (req, res) => {
   //   res.status(400).send("Invalid request. 'id' is missing in the request body.");
   // }
 });
+
+// ----------------------------------------------------------------------
+
+app.post('/register', (req, res) => {
+  const { user } = req.body;
+  const data = fs.readFileSync('users.json', 'utf-8', (error) => {
+    res.send(error)
+  });
+
+
+
+  const parsedData = data.length > 0 ? JSON.parse(data) : [];
+
+  const exist = parsedData.filter((currentUser) => currentUser.email === user.email).length > 0;
+
+  if (exist) {
+    res.send('User Already Registered!!')
+    return;
+  }
+
+  parsedData.push(user)
+
+  fs.writeFileSync('users.json', JSON.stringify(parsedData), (error) => {
+    console.log(error)
+  })
+
+  res.send(user)
+});
+
+app.get('/getusers', (req, res) => {
+  const data = fs.readFileSync('users.json', 'utf-8', (error) => {
+    res.send(error)
+  });
+  res.send(JSON.parse(JSON.stringify(data)))
+})
+
+app.post('/login', (req, res) => {
+  const { user } = req.body;
+  const resp = users.filter((regUser) => {
+    var result = null;
+    if (regUser.email !== user.email)
+      result = 'User not Found! Please Register';
+    else if (regUser.password !== user.password)
+      result = 'Invalid Password';
+    else if (regUser.email !== user.email && regUser.password !== user.password)
+      result = 'Invalid Data';
+    else
+      result = user;
+    return result;
+  })
+
+  console.log(resp)
+
+  res.send(resp[0]);
+})
 
 
 
